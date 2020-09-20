6d40e43750e3f6d95fa01f1b5882168ba5e257aa
Joan Pedro <jpos.senai@gmail.com>
2020-09-20 20:52:31 -0300

    feat: add constraint validation to unique username

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,11 +1,13 @@
-import { IsString, Matches, MaxLength, MinLength } from "class-validator"
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator"
 
 export class AuthCredentialsDTO {
+  @IsNotEmpty()
   @IsString()
   @MinLength(4)
   @MaxLength(20)
   username: string
 
+  @IsNotEmpty()
   @IsString()
   @MinLength(8)
   @MaxLength(20)
--- src/auth/repositories/user.repository.ts
+++ src/auth/repositories/user.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDTO } from "../dto/auth-credentials.dto";
 import { UserEntity } from "../entities/user.entity";
@@ -10,11 +10,21 @@ export class UserRepository extends Repository<UserEntity> {
   async singUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
     const { username, password } = authCredentialsDTO
     
+    const exists = this.findOne({ username })
+
+    if (exists) {
+      throw new ConflictException('Username already exists')
+    }
+
     const user = new UserEntity()
-    
     user.username = username
     user.password = password
-    await user.save()
+    
+    try {
+      await user.save()
+    } catch (error) {
+      throw new InternalServerErrorException()
+    }
 
     return null
   }
