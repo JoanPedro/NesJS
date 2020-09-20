60a64534ad6e3b2694a83d4d4b9b6bbf4bbe2abb
Joan Pedro <jpos.senai@gmail.com>
2020-09-20 10:51:16 -0300

    feat: TypeORM configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './database/config/typeorm.config';
 import { TasksModule } from './tasks';
 
 @Module({
-  imports: [TasksModule]
+  imports: [
+    TypeOrmModule.forRoot(TypeOrmConfig),
+    TasksModule
+  ]
 })
 export class AppModule {}
---
+++ src/database/config/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm"
+
+export const TypeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mssql',
+  host: 'localhost',
+  port: 1433,
+  username: 'sa',
+  password: 'yourStrong(!)Password',
+  database: 'NestJS',
+  entities: [__dirname + '/../../**/*.entity.ts'],
+  migrations: [__dirname + '/../migrations'],
+  cli: {
+    migrationsDir: __dirname + '/../migrations',
+    entitiesDir: __dirname + '/../entities',
+    subscribersDir: __dirname + '/../subscribers'
+  },
+  synchronize: true
+}
