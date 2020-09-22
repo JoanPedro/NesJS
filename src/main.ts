f8b8778a1502494b7eacbbb24812c3d428faabfb
Joan Pedro <jpos.senai@gmail.com>
2020-09-22 10:20:57 -0300

    chore: configuration management setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common'
 import { AppModule } from './app.module';
+import * as config from 'config'
 
 async function bootstrap() {
   const PORT = 3000
   const logger = new Logger('bootstrap')
   const app = await NestFactory.create(AppModule)
+
+  const serverConfig = config.get('server')
+  console.log(serverConfig)
+
   await app.listen(PORT);
   logger.log(`Application listening on port: ${PORT}`)
 }
